63170d5f0ccc9199cb6bce7a631781d42e1eba66
Endy WindAflame <dev.windaflame@protomail.com>
2023-02-28 09:41:18 +0100

    Add first TP

---
+++ tp1/lib/data_set.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<Tache> liste_tache = [];
+  liste_tache.add(Course("Kinder Bueno"));
+  liste_tache.add(Sport("Triathlon"));
+  liste_tache.add(Course("KitKat"));
+  liste_tache.add(Sport("Trail"));
+  liste_tache.add(Course("Nesquik"));
+  liste_tache.add(Course("Mars"));
+  liste_tache.add(Course("Twix"));
+  liste_tache.add(Sport("Marathon"));
+  liste_tache.add(Course("Bounty"));
+}
